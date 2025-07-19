c2fef774a132b78c08b6df0c48367325ca8c3d21
Cal Crosby <calcruiseby@gmail.com>
2025-07-19 04:43:29 +0000

    added e2e test component for soundcron list select

--- e2e/soundcron_list_test.go
+++ e2e/soundcron_list_test.go
@@ -83,66 +83,103 @@ func TestSoundCronList_NoSoundCrons(t *testing.T) {
 	}
 }
 
-func TestSoundCronList_HappyPath(t *testing.T) {
+var soundCronListSlashCommandInteraction = &discordgo.InteractionCreate{
+	Interaction: &discordgo.Interaction{
+		Type: discordgo.InteractionApplicationCommand,
+		Data: discordgo.ApplicationCommandInteractionData{
+			Name: "soundcron",
+			Options: []*discordgo.ApplicationCommandInteractionDataOption{
+				{
+					Name:  "list",
+					Type:  discordgo.ApplicationCommandOptionSubCommand,
+					Value: "list",
+				},
+			},
+		},
+		GuildID: "74241007174813750",
+	},
+}
+
+var expectedSoundCronListResponse = &discordgo.InteractionResponse{
+	Type: discordgo.InteractionResponseChannelMessageWithSource,
+	Data: &discordgo.InteractionResponseData{
+		Content: "Current SoundCrons",
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.Button{
+						Label:    "Take On Me (A-ha)",
+						Style:    discordgo.SecondaryButton,
+						CustomID: "soundcron_select_menu:determinism:8597e24a-f204-4c88-bad0-fe0ab9a73ff1",
+					},
+				},
+			},
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.Button{
+						Label:    "Everything She Wants (Wham!)",
+						Style:    discordgo.SecondaryButton,
+						CustomID: "soundcron_select_menu:determinism:302808d9-141e-410d-a69d-2418ad15b5de",
+					},
+				},
+			},
+		},
+	},
+}
+
+var soundCronListButtonInteraction = &discordgo.InteractionCreate{
+	Interaction: &discordgo.Interaction{
+		Type: discordgo.InteractionMessageComponent,
+		Data: discordgo.MessageComponentInteractionData{
+			CustomID: "soundcron_select_menu:determinism:8597e24a-f204-4c88-bad0-fe0ab9a73ff1",
+			ComponentType: discordgo.ButtonComponent,
+		},
+		GuildID: "74241007174813750",
+	},
+}
+
+var expectedSoundCronListButtonResponse = &discordgo.InteractionResponse{
+	Type: discordgo.InteractionResponseChannelMessageWithSource,
+	Data: &discordgo.InteractionResponseData{
+		Content: "Take On Me (A-ha)",
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.Button{
+						Label: "Edit",
+						Style: discordgo.SecondaryButton,
+						CustomID: "soundcron_edit:determinism",
+					},
+					discordgo.Button{
+						Label: "Delete",
+						Style: discordgo.DangerButton,
+						CustomID: "soundcron_delete:determinism",
+					},
+				},
+			},
+		},
+	},
+}
+					  
+
+func TestSoundCronList(t *testing.T) {
 	connStr := e2e.UsePostgres(t)
 	repo := e2e.GetRepository(t, connStr)
 	seedTestData(t, repo)
 
-	t.Run("lists soundcrons with select menu", func(t *testing.T) {
-		slashCommandInteraction := &discordgo.InteractionCreate{
-			Interaction: &discordgo.Interaction{
-				Type: discordgo.InteractionApplicationCommand,
-				Data: discordgo.ApplicationCommandInteractionData{
-					Name: "soundcron",
-					Options: []*discordgo.ApplicationCommandInteractionDataOption{
-						{
-							Name:  "list",
-							Type:  discordgo.ApplicationCommandOptionSubCommand,
-							Value: "list",
-						},
-					},
-				},
-				GuildID: "74241007174813750",
-			},
-		}
+	handler := handler.NewInteractionHandler(repo, nil, &determinsticIDGenerator{})
+	session := &mockSession{}
 
-		session := &mockSession{}
+	handler(session, soundCronListSlashCommandInteraction)
 
-		handler := handler.NewInteractionHandler(repo, nil, &determinsticIDGenerator{})
-		handler(session, slashCommandInteraction)
+	diff := cmp.Diff(expectedSoundCronListResponse, session.Resp)
+	if diff != "" {
+		t.Errorf("session mismatch (-want +got):\n%s", diff)
+	}
 
-		expected := &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Current SoundCrons",
-				Components: []discordgo.MessageComponent{
-					discordgo.ActionsRow{
-						Components: []discordgo.MessageComponent{
-							discordgo.Button{
-								Label:    "Take On Me (A-ha)",
-								Style:    discordgo.SecondaryButton,
-								CustomID: "soundcron_select_menu:determinism:8597e24a-f204-4c88-bad0-fe0ab9a73ff1",
-							},
-						},
-					},
-					discordgo.ActionsRow{
-						Components: []discordgo.MessageComponent{
-							discordgo.Button{
-								Label:    "Everything She Wants (Wham!)",
-								Style:    discordgo.SecondaryButton,
-								CustomID: "soundcron_select_menu:determinism:302808d9-141e-410d-a69d-2418ad15b5de",
-							},
-						},
-					},
-				},
-			},
-		}
-
-		diff := cmp.Diff(expected, session.Resp)
-		if diff != "" {
-			t.Errorf("session mismatch (-want +got):\n%s", diff)
-		}
-	})
-
-	t.Run("shows actions when soundcron is selected", func(t *testing.T) {})
+	handler(session, soundCronListButtonInteraction)
+	diff = cmp.Diff(expectedSoundCronListButtonResponse, session.Resp)
+	if diff != "" {
+		t.Errorf("session mismatch (-want +got):\n%s", diff)
+	}
 }
